3d6166cfd95df59990974bda2db893062e365106
Абдулбасырова Альбина <kuzina.seledka@gmail.com>
2025-08-03 10:54:50 +0500

    Добавлено нижнее меню

--- app/src/main/java/com/example/scorochtenie2/MainActivity.kt
+++ app/src/main/java/com/example/scorochtenie2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+        
+        setupBottomNavigation()
+        setupWindowInsets()
+    }
+    
+    private fun setupBottomNavigation() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> true
+                R.id.navigation_learning -> true
+                R.id.navigation_practice -> true
+                R.id.navigation_progress -> true
+                R.id.navigation_settings -> true
+                else -> false
+            }
+        }
+    }
+    
+    private fun setupWindowInsets() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNavigation) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, 0, 0, systemBars.bottom)
             insets
         }
     }
